f64a500e0022e4b68410a13ca7a889906b6bac63
SilverCatKawaii <mikanmc77772@gmail.com>
2024-11-04 23:59:41 +0900

    おすすめのbatとtxt追加

---
+++ start.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Java\jdk-17\bin\java.exe" -jar PikachuObfuscator.jar input.jar input-obf.jar -asmVer 9 -fixVersion -classToFolder -useStringObf -useStringObfT -useNumberObf -useJunkCode -inClass (.*) -exClass net.shoreline.client.mixin(.*) net.shoreline.client.socket.json(.*) -obfLocalVar -delLocalVar -applymap maps.txt
