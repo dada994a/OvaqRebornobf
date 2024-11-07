ed392b60e00645f4d73101006e2df0f010e5a08b
SilverCatKawaii <mikanmc77772@gmail.com>
2024-11-07 16:55:04 +0900

    fast

--- src/main/java/devs/pikachu/protect/transformer/impl/ReverseTransformer.java
+++ src/main/java/devs/pikachu/protect/transformer/impl/ReverseTransformer.java
@@ -5,7 +5,6 @@ import devs.pikachu.protect.utility.ClassWriter1;
 import devs.pikachu.protect.utility.Utils;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Opcodes;
 
 public class ReverseTransformer extends TransVisitor {
     public ReverseTransformer(ClassReader cr, ClassWriter1 cw, int api) {
@@ -18,11 +17,11 @@ public class ReverseTransformer extends TransVisitor {
         return new MethodVisitor(api, mv) {
             @Override
             public void visitInsn(int opcode) {
-                if (opcode >= Opcodes.ICONST_M1 && opcode <= Opcodes.ICONST_5) {
-                    int value = opcode - Opcodes.ICONST_0;
+                if (opcode >= 2 && opcode <= 8) {
+                    int value = opcode - 3;
                     String string = Utils.reverse(value);
                     mv.visitLdcInsn(string);
-                    mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/String", "hashCode", "()I", false);
+                    mv.visitMethodInsn(182, "java/lang/String", "hashCode", "()I", false);
                 } else {
                     super.visitInsn(opcode);
                 }
